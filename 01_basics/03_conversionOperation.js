efdd7eb2c94dd22360b7c402f2c9302f407f7391
ATUL KUMAR <119154306+theatulkumar@users.noreply.github.com>
2023-12-18 11:35:52 +0530

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+// let score = 33;
+// let score1 = "33abc";
+// console.log(typeof score);
+// console.log(typeof(score1));
+// let valueInNumber = Number(score1);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
